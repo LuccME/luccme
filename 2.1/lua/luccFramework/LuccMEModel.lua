ff82810aecf7536a00de25f1971ebc78ab276f78
Diego Melo <melo.diego@gmail.com>
2015-05-26 19:59:06 +0000

    Added verifies for the inputs coming from the main file

--- 2.1/lua/luccFramework/LuccMEModel.lua
+++ 2.1/lua/luccFramework/LuccMEModel.lua
@@ -55,7 +55,7 @@ function LuccMEModel(model)
 		end
 		
 		self.demand:execute(event, model)
-	    self.potential:execute(event, model)
+	  self.potential:execute(event, model)
 		self.allocation:execute(event, model)
 	end
 
@@ -64,18 +64,38 @@ function LuccMEModel(model)
 	-- @arg event An Event represents a time instant when the simulation engine must execute some computation.
 	-- @usage luccMeModel:verify(event)
 	model.verify = function(self, event)
+	  -- Verify the model name
 		if (model.name == nil) then
 			error("Model name not defined", 2)
 		end
 	
+	  -- Verify the scenario name
 		if (self.scenarioName == nil and self.scenarioStartTime ~= nil) then
 			error("A scenario name is required", 2)
 		end
-	
+	 
+	  -- Verify the scenario start time
 		if (self.scenarioName ~= nil and self.scenarioStartTime == nil) then
 			error("A scenario start time is required", 2)
 		end
 	
+	  -- Verify the scenario stop time
+    if (self.scenarioName ~= nil and self.scenarioEndTime == nil) then
+      error("A scenario end time is required", 2)
+    end
+
+    -- Verify the cellular space
+    if (not self.cs) then
+      error("A Cellular Spae must be defined", 2)
+    end
+
+    -- Verify whether the land use no data is declared and its valid
+    if(self.landUseNoData == nil) then
+      error("Land use no data type is missing", 2)
+    elseif(self.cs.cells[1][self.landUseNoData] == nil) then
+      error("Invalid land use no data type", 2)
+    end
+    
 		self.result = {}
 
 		-- Verify whether the attributes to be saved were calculated in the model
@@ -108,6 +128,22 @@ function LuccMEModel(model)
 		end
 		
 		io.flush()
+		
+		-- Verify wheter the demand compontent was declared
+    if (not self.demand) then
+        error("A demand component must be specified", 2)
+    end
+    
+    -- Verify wheter the potential compontent was declared
+    if (not self.potential) then
+      error("A potential component must be specified", 2)     
+    end
+		
+		-- Verify wheter the allocation compontent was declared
+    if (not self.allocation) then
+      error("An allocation component must be specified", 2)     
+    end
+		
 		self.demand:verify(event, self)
 		self.potential:verify(event, self)
 		self.allocation:verify(event, self)
@@ -126,38 +162,38 @@ function LuccMEModel(model)
 				print("Updating dynamic variables...")
 				if ((self.scenarioStartTime ~= nil) and (currentTime >= self.scenarioStartTime)) then
 				    cs_temp = CellularSpace {	host = self.cs.host, user = self.cs.user, password = self.cs.password,
-												database = self.cs.database,
-												theme = self.cs.theme.."_"..self.scenarioName.."_"..updtYear
-											}
+              												database = self.cs.database,
+              												theme = self.cs.theme.."_"..self.scenarioName.."_"..updtYear
+											              }
 					print (self.cs.theme.."_"..self.scenarioName.."_"..updtYear)
 				else
 					cs_temp = CellularSpace {	host = self.cs.host, user = self.cs.user, password = self.cs.password,
-				      							database = self.cs.database,
-												theme = self.cs.theme.."_"..updtYear
-											}
+				      							        database = self.cs.database,
+												            theme = self.cs.theme.."_"..updtYear
+											            }
 					print (self.cs.theme.."_"..updtYear)
 				end
 
 				-- for each cell in the original cs, variables are contained in cs_temp is updated
 				local flag = false
 			       		
-			    forEachCellPair(cs, cs_temp, function(cell, cell_temp)
-													for var, value in pairs (cell_temp) do
-														if (var ~= "cObj_" and var ~= "objectId_" and
-															var ~= "y" and var ~= "x" and var ~= "past" and
-															var ~= "agents" and var ~= "agents_" and
-															var ~= "object_id_") then
-																if (cell[var] ~= nil) then
-																	cell[var] = cell_temp[var]
-																	if (flag == false) then
-																		print("          ", var, cell[var])
-																	end		          					
-																end
-														end --1st inner if
-													end --for var
-													flag = true
-											 end --function
-								)
+		    forEachCellPair(cs, cs_temp, function(cell, cell_temp)
+              													for var, value in pairs (cell_temp) do
+              														if (var ~= "cObj_" and var ~= "objectId_" and
+              															var ~= "y" and var ~= "x" and var ~= "past" and
+              															var ~= "agents" and var ~= "agents_" and
+              															var ~= "object_id_") then
+              																if (cell[var] ~= nil) then
+              																	cell[var] = cell_temp[var]
+              																	if (flag == false) then
+              																		print("          ", var, cell[var])
+              																	end		          					
+              																end
+              														end --1st inner if
+              													end --for var
+              													flag = true
+              											 end --function
+							          )
 			end
 		end				
 	end

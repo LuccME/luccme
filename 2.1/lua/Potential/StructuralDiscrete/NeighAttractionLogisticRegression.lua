f2898c33950184686c5ed657b19cf04b76165fe0
Diego Melo <melo.diego@gmail.com>
2015-06-22 18:47:37 +0000

    Added @usage and verify method

--- 2.1/lua/Potential/StructuralDiscrete/NeighAttractionLogisticRegression.lua
+++ 2.1/lua/Potential/StructuralDiscrete/NeighAttractionLogisticRegression.lua
@@ -15,23 +15,29 @@
 -- @arg component.calcRegressionLogistic Handles with the calculation of the regression.
 -- @arg component.probability Compute the probability logistic method of a NeighAttractionLogisticRegression component.
 -- @return The modified component.
--- @usage myPontencial = NeighAttractionLogisticRegression {
--- regressionData =	{
--- 					-- Region 1
---					{ const  = -0.1,
---					  betas = {beta1 =	-0.05, beta2 =	0.2, beta3 = 0.1},
---					  elasticity = 0.5, percNeighborsUse = 0.5},
---					-- Region 2
---					{ isLog = false, error = 0.3, const  = -0.3,
---					  betas = {beta1 =	0.03, beta2 =	0.6, beta3 = 0.01},
---					  elasticity = 0.1, percNeighborsUse = 0.2},
---					-- Region 3
---					{ isLog = false, error = 0, const  = 0,
---					  betas = {beta1 =	0},
---					  elasticity = 0.5, percNeighborsUse = 0.5},
---					},
---					landUseDrivers = {},
---}
+-- @usage potential  =  NeighAttractionLogisticRegression {
+--    regressionData = { { --  Region 1
+--                        -- D
+--                        { const = -0.1004, 
+--                          betas = {beta1 = 0.05814, beta2 = -0.97500, beta3 = -2.51651},                
+--                          elasticity =  0.5, 
+--                          percNeighborsUse = 0.5
+--                        },
+--                        -- F
+--                        { const = -2.34188, 
+--                          betas = {beta1 = -0.02727, beta2 = -4.30977, beta3 = -3.10320},                                                              
+--                          elasticity =  0.5, 
+--                          percNeighborsUse = 0.5
+--                        },
+--                        -- O
+--                        { const = 0,
+--                          betas = {beta1 = 1},  
+--                          elasticity = 1,
+--                          percNeighborsUse = 0.5
+--                        }   
+--                       }  --end region 1 
+--                      } -- end regressionData
+--  }
 function NeighAttractionLogisticRegression(component)
 	--- Handles with the execution method of a NeighAttractionLogisticRegression component.
 	-- @arg self A NeighAttractionLogisticRegression component.
@@ -96,8 +102,58 @@ function NeighAttractionLogisticRegression(component)
 	-- @arg self A NeighAttractionLogisticRegression component.
 	-- @arg event A representation of a time instant when the simulation engine must execute.
 	-- @usage self.potential:verify(event, self)
-	component.verify = function(self, event)
-	end
+	component.verify = function(self, event, modelParameters)
+    -- check regressionData
+    if (self.regressionData == nil) then
+      error("regressionData is missing", 2)
+    end    
+    
+    local regionsNumber = #self.regressionData
+
+    -- check number of Regions
+    if (regionsNumber == nil or regionsNumber == 0) then
+      error("The model must have at least One region", 2)
+    else
+      for i = 1, regionsNumber, 1 do
+        local regressionNumber = #self.regressionData[i]
+        local lutNumber = #modelParameters.landUseTypes
+        
+        -- check the number of regressions
+        if (regressionNumber ~= lutNumber) then
+          error("Invalid number of regressions on Region number "..i.." . Regressions: "..regressionNumber.." LandUseTypes: "..lutNumber, 2)
+        end
+        
+        for j = 1, regressionNumber, 1 do
+          -- check constant variable
+          if(self.regressionData[i][j].const == nil) then
+            error("const variable is missing on Region "..i.." LandUseType number "..j, 2)
+          end
+         
+          -- check elasticity variable
+          if (self.regressionData[i][j].elasticity == nil) then
+            error("elasticity variable is missing on Region "..i.." LandUseType number "..j, 2)
+          end
+          
+          -- check percNeighborsUse variable
+          if (self.regressionData[i][j].percNeighborsUse == nil) then
+            error("percNeighborsUse variable is missing on Region "..i.." LandUseType number "..j, 2)
+          end
+         
+          -- check betas variable
+          if (self.regressionData[i][j].betas == nil) then
+            error("betas variable is missing on Region "..i.." LandUseType number "..j, 2)
+          end
+          
+          -- check betas within database
+          for k, lu in pairs (self.regressionData[i][j].betas) do
+            if (modelParameters.cs.cells[1][k] == nil) then
+              error("Beta "..k.." on Region "..i.." LandUseType number "..j.." not found within database", 2)
+            end
+          end
+        end -- for j
+      end -- for i
+    end -- else
+  end -- verify
 	
 	--- Handles with the calculation of the regression.
 	-- logistic method of a NeighAttractionLogisticRegression component.

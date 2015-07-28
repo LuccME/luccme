67322eee9a29e5f323fea57903eeeae675572d68
Diego Melo <melo.diego@gmail.com>
2015-07-28 14:14:18 +0000

    Back the Continuous Components to non region. (it has failure on the tests)

--- 2.1/lua/Allocation/StructuralContinuous/init.lua
+++ 2.1/lua/Allocation/StructuralContinuous/init.lua
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------------------
 --LuccME - a framework for top-down land use change modelling.
---Copyright © 2009 - 2011 INPE.
+--Copyright © 2009 - 2015 INPE.
 --
 --This code is part of the LuccME framework.
 --This framework is a free software; you can redistribute and/or

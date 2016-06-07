78eef946feae60f60887fb884ff536a3eaa7a63c
Diego Melo <melo.diego@gmail.com>
2016-06-07 19:41:05 +0000

    Running version

--- 3.0/lua/AttractRepulseLogisticRegression.lua
+++ 3.0/lua/AttractRepulseLogisticRegression.lua
@@ -1,30 +1,85 @@
--------------------------------------------------------------------------------------------
---LuccME - a framework for topdown land use change modeling.
---Copyright © 2009 - 2011 INPE.
+--- Modification of the LogisticRegression  combining cellular automata based models ideas. Cell potential is modified 
+-- according to the attractiveness of the same class in a given (generic) neighbourhood.
+-- @arg component A NeighAttractionLogisticRegression component.
+-- @arg component.potentialData A table with the regression parameters for each attribute.
+-- @arg component.potentialData.const A linear regression constant.
+-- @arg component.potentialData.elasticity An elasticity value, closer to 1 is more difficulty
+-- to transition for other land uses.
+-- @arg component.potentialData.percNeighborsUse Percent of neighbours with the same use.
+-- @arg component.potentialData.betas A linear regression betas for land use drivers
+-- and the index of landUseDrivers to be used by the regression (attributes).
+-- @arg component.run Handles with the execution method of a NeighAttractionLogisticRegression component.
+-- @arg component.verify Handles with the verify method of a NeighAttractionLogisticRegression component.
+-- @arg component.calcRegressionLogistic Handles with the calculation of the regression.
+-- @arg component.probability Compute the probability logistic method of a NeighAttractionLogisticRegression component.
+-- @return The modified component.
+-- @usage --DONTRUN 
+--P1 = AttractRepulseLogisticRegression
+--{
+--  potentialData =
+--  {
+--    -- region 1
+--    {
+--      -- floresta
+--      {
+--        const = -1.961,
+--        elasticity = 0.1,
+--        percNeighborsUse = 0.5,
 --
---This code is part of the LuccME framework.
---This framework is a free software; you can redistribute and/or
---modify it under the terms of the GNU Lesser General Public
---License as published by the Free Software Foundation; either
---version 3 of the License, or (at your option) any later version.
+--        betas =
+--        {
+--          dist_rodovias = 0.00008578,
+--          assentamentos = -0.2604,
+--          uc_us = 0.6064,
+--          fertilidadealtaoumedia = 0.4393
+--        }
+--      },
 --
---You should have received a copy of the GNU Lesser General Public
---License along with this library.
+--      -- desmatamento
+--      {
+--        const = 1.978,
+--        elasticity = 0.1,
+--        percNeighborsUse = 0.5,
 --
---The authors reassure the license terms regarding the warranties.
---They specifically disclaim any warranties, including, but not limited to,
---the implied warranties of merchantability and fitness for a particular purpose.
---The framework provided hereunder is on an "as is" basis, and the authors have no
---obligation to provide maintenance, support, updates, enhancements, or modifications.
---In no event shall INPE be held liable to any part for direct,
---indirect, special, incidental, or consequential damages arising out of the use
---of this library and its documentation.
+--        betas =
+--        {
+--          dist_rodovias = -0.00008651,
+--          assentamentos = 0.2676,
+--          uc_us = -0.6376,
+--          fertilidadealtaoumedia = -0.4565
+--        }
+--      },
 --
--------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------
-
+--      -- outros
+--      {
+--        const = 0,
+--        elasticity = 0,
+--        percNeighborsUse = 0,
+--
+--        betas =
+--        {
+--          
+--        }
+--      }
+--    }
+--  },
+--  
+--  affinityMatrix = 
+--  {
+--    -- Indicates the affinity between the uses :
+--    -- 0 <  m_ij <= 1, for positive values the use "i" is attracted by the use "j"
+--    --  -1 <= m_ij <  0, for negative values the use "i" is repelled by the use "j"
+--    --  m_ij =  0, Indifferent
+--    -- region 1
+--    { 
+--      {0, 0, 0},
+--      {0, 0, 0},
+--      {0, 0, 0}
+--    } 
+--  }
+--}
 function AttractRepulseLogisticRegression(component)
-	--- Handles with the execution method of a NeighAttractionLogisticRegression component.
+	--- Handles with the execution method of a AttractRepulseLogisticRegression component.
 	-- @arg event A representation of a time instant when the simulation engine must run.
 	-- @arg luccMEModel A luccME Model.
 	-- @usage --DONTRUN
@@ -32,43 +87,45 @@ function AttractRepulseLogisticRegression(component)
 	component.run = function(self, event, luccMEModel)
 		local cs = luccMEModel.cs 
 		local luTypes = luccMEModel.landUseTypes 
-		local regressionData = self.regressionData 
+		local potentialData = self.potentialData 
  		local landUseDrivers = self.landUseDrivers 
-  		local filename = self.filename 
+		local filename = self.filename
+		local cellType = 0
+		local w = 0
+		local d = 0 
 		
 		if (filename ~= nil) then
 			loadGALNeighborhood(filename)
 		else
-		  if(event:getTime() == luccMEModel.startTime) then
-			cs:createNeighborhood()   
+		  if (event:getTime() == luccMEModel.startTime) then
+			   cs:createNeighborhood()   
 		  end
 		end
  		
 		local totalNeigh = 0 
   		
-  		for k, cell in pairs (cs.cells) do
-			-- Initialization
+		for k, cell in pairs (cs.cells) do
 			totalNeigh = #cell:getNeighborhood() - 1
 			
-			for i, inputValues in  pairs (regressionData[cell.region]) do
+			for i, inputValues in pairs (potentialData[cell.region]) do
 				local lu = luTypes[i]
-				if (cell[lu] == 1)  then		
-					cellType = toIndex(lu ,luTypes)
+				if (cell[lu] == 1)  then
+					cellType = self.toIndex(lu, luTypes)
 				end
 				cell["tau_"..lu] = 0.0
 			end
 
 			-- "TAU": ATTRACTION/REPULSION FACTOR			
-			for luind, inputValues in  pairs( regressionData[cell.region]) do
+			for luind, inputValues in pairs (potentialData[cell.region]) do
 				local lu = luTypes[luind]
 				-- Step 1: Count neighbgours		
 				local numNeigh = 0;
 				forEachNeighbor(cell, function(cell, neigh)			
-										if (neigh[lu] == 1 and neigh ~= cell)  then		
-											numNeigh = numNeigh + 1
-										end						
-									 end
-								)
+            										if (neigh[lu] == 1 and neigh ~= cell)  then		
+            											numNeigh = numNeigh + 1
+            										end						
+            									 end
+        								)
 				
 				-- Step 2: Compute weight 
 				d = inputValues.percNeighborsUse * totalNeigh
@@ -89,16 +146,15 @@ function AttractRepulseLogisticRegression(component)
 				end
 				
 				-- Step 5: Calculates the regression estimates 
-				local regrProb = model.calcRegressionLogistic(cell, inputValues, landUseDrivers, self)
+				local regrProb = self.calcRegressionLogistic(cell, inputValues, self)
 				
 				-- Step 6: Compute the potential
 				cell[lu.."_pot"] = regrProb + elas
-
 			end -- use type
 		end -- end cells
 	end -- end execute
 
-	--- Handles with the verify method of a NeighAttractionLogisticRegression component.
+	--- Handles with the verify method of a AttractRepulseLogisticRegression component.
 	-- @arg event A representation of a time instant when the simulation engine must run.
 	-- @arg luccMEModel A luccME Model.
 	-- @usage --DONTRUN
@@ -118,56 +174,74 @@ function AttractRepulseLogisticRegression(component)
 		  error("The model must have at least One region", 2)
 		else
 		  for i = 1, regionsNumber, 1 do
-			local regressionNumber = #self.potentialData[i]
-			local lutNumber = #luccMEModel.landUseTypes
-			local activeRegionNumber = 0
-			
-			-- check the number of regressions
-			if (regressionNumber ~= lutNumber) then
-			  error("Invalid number of regressions on Region number "..i.." . Regressions: "..regressionNumber.." LandUseTypes: "..lutNumber, 2)
-			end
-			
-			-- check active regions
-			for k,cell in pairs (cs.cells) do
-			  if (cell.region == nil) then
-				cell.region = 1
-			  end         
-			  if (cell.region == i) then
-				activeRegionNumber = i
-			  end
-			end 
-			if (activeRegionNumber == 0) then
-			  error("Region ".. i.." is not set into database.")  
-			end
-					
-			for j = 1, regressionNumber, 1 do
-			  -- check constant variable
-			  if(self.potentialData[i][j].const == nil) then
-				error("const variable is missing on Region "..i.." LandUseType number "..j, 2)
-			  end
-			 
-			  -- check elasticity variable
-			  if (self.potentialData[i][j].elasticity == nil) then
-				error("elasticity variable is missing on Region "..i.." LandUseType number "..j, 2)
-			  end
-			  
-			  -- check percNeighborsUse variable
-			  if (self.potentialData[i][j].percNeighborsUse == nil) then
-				error("percNeighborsUse variable is missing on Region "..i.." LandUseType number "..j, 2)
-			  end
-			 
-			  -- check betas variable
-			  if (self.potentialData[i][j].betas == nil) then
-				error("betas variable is missing on Region "..i.." LandUseType number "..j, 2)
-			  end
-			  
-			  -- check betas within database
-			  for k, lu in pairs (self.potentialData[i][j].betas) do
-				if (luccMEModel.cs.cells[1][k] == nil) then
-				  error("Beta "..k.." on Region "..i.." LandUseType number "..j.." not found within database", 2)
-				end
-			  end
-			end -- for j
+  			local regressionNumber = #self.potentialData[i]
+  			local lutNumber = #luccMEModel.landUseTypes
+  			local activeRegionNumber = 0
+  			
+  			-- check the number of regressions
+  			if (regressionNumber ~= lutNumber) then
+  			  error("Invalid number of regressions on Region number "..i.." . Regressions: "..regressionNumber.." LandUseTypes: "..lutNumber, 2)
+  			end
+  			
+  			-- check active regions
+  			for k,cell in pairs (cs.cells) do
+  			  if (cell.region == nil) then
+  				  cell.region = 1
+  			  end         
+  			  if (cell.region == i) then
+  				  activeRegionNumber = i
+  			  end
+  			end 
+  			
+  			if (activeRegionNumber == 0) then
+  			  error("Region ".. i.." is not set into database.")  
+  			end
+  					
+  			for j = 1, regressionNumber, 1 do
+  			  -- check constant variable
+  			  if(self.potentialData[i][j].const == nil) then
+  				  error("const variable is missing on Region "..i.." LandUseType number "..j, 2)
+  			  end
+  			 
+  			  -- check elasticity variable
+  			  if (self.potentialData[i][j].elasticity == nil) then
+  				  error("elasticity variable is missing on Region "..i.." LandUseType number "..j, 2)
+  			  end
+  			  
+  			  -- check percNeighborsUse variable
+  			  if (self.potentialData[i][j].percNeighborsUse == nil) then
+  				  error("percNeighborsUse variable is missing on Region "..i.." LandUseType number "..j, 2)
+  			  end
+  			 
+  			  -- check betas variable
+  			  if (self.potentialData[i][j].betas == nil) then
+  				  error("betas variable is missing on Region "..i.." LandUseType number "..j, 2)
+  			  end
+  			  
+  			  -- check betas within database
+  			  for k, lu in pairs (self.potentialData[i][j].betas) do
+    				if (luccMEModel.cs.cells[1][k] == nil) then
+    				  error("Beta "..k.." on Region "..i.." LandUseType number "..j.." not found within database", 2)
+    				end
+  			  end
+  			  
+  			  local affinityMatrix = #self.affinityMatrix[i]
+          local lutNumber = #luccMEModel.landUseTypes
+          
+          -- check the number of transitions
+          if (affinityMatrix ~= lutNumber) then
+            error("Invalid number of transitions on Region number "..i..". Transitions: "..affinityMatrix.." LandUseTypes: "..lutNumber, 2)
+          end
+          
+          for w = 1, affinityMatrix, 1 do
+            for k = 1, lutNumber, 1 do  
+              -- check the matrix values
+              if(self.affinityMatrix[i][w][k] < -1 or self.affinityMatrix[i][w][k] > 1) then
+                error("Invalid data on transitionMatrix: "..self.affinityMatrix[i][w][k]..". Region: "..i.. " Position: "..w.."x"..k..". The acceptable values are 0 or 1", 2)
+              end
+            end -- for k
+          end -- for w
+  			end -- for j
 		  end -- for i
 		end -- else
 	end -- verify
@@ -177,12 +251,11 @@ function AttractRepulseLogisticRegression(component)
 	-- @arg usetypes A set of land use type.
 	-- @usage --DONTRUN
 	-- component.toIndex(lu, luTypes)
-	component.toIndex = function(self, lu, usetypes)
-		local index = 0
+	component.toIndex = function(lu, usetypes)
 		for i, value in  pairs (usetypes) do
 		  if (value == lu) then
-			index = i
-			break
+  			index = i
+  			break
 		  end   
 		end
 		
@@ -198,7 +271,6 @@ function AttractRepulseLogisticRegression(component)
 	component.calcRegressionLogistic = function(cell, inputValues, component)
 		local regrLogit = inputValues.const
 		local betas = inputValues.betas
-		local attrs = inputValues.attributes
 	
 		for var, beta in pairs (betas) do
 			regrLogit = regrLogit + beta * cell[var]
@@ -214,11 +286,11 @@ function AttractRepulseLogisticRegression(component)
 	component.probability = function(z)
 		local euler  = 2.718281828459045235360287
 		local zEuler = math.pow(euler, z)
-		local prob = zEuler/(1 + zEuler)
-			
+		local prob = zEuler / (1 + zEuler)
+
 		return prob
 	end
 	
 	collectgarbage("collect")
 	return component
-end --close NeighAttractionLogisticRegression
+end --close AttractRepulseLogisticRegression
